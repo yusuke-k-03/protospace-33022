52d0e0eb8e542207c8ff8a633a03124bc85aa1e1
yusuke-k-03 <sinkuku01@gmail.com>
2020-12-25 15:38:25 +0900

    commentモデル、テーブル作成

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,4 +7,5 @@ class Prototype < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ApplicationRecord
   validates :position, presence: true
 
   has_many :prototypes
+  has_many :comments
 end
